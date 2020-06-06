48fe4c264c101e9d7dd18407eb264f62ce7f1b31
2547technowizz <neil_edward_lobo@hotmail.com>
2020-06-06 11:41:16 -0400

    edited driver account screen

--- components/DelivererAccount.js
+++ components/DelivererAccount.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "native-base";
+import { View, StyleSheet } from "react-native";
 
 class DelivererAccount extends React.Component {
 	state = {};
@@ -9,3 +9,11 @@ class DelivererAccount extends React.Component {
 }
 
 export default DelivererAccount;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
