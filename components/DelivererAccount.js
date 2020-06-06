dc6a60f064a62e57d2af6b5be82f1dba867e406a
2547technowizz <neil_edward_lobo@hotmail.com>
2020-06-06 11:05:31 -0400

    created drivers account page

---
+++ components/DelivererAccount.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "native-base";
+
+class DelivererAccount extends React.Component {
+	state = {};
+	render() {
+		return <View></View>;
+	}
+}
+
+export default DelivererAccount;
